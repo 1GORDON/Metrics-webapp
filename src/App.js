f5bc6c7676404b875a33bf61907635edece4dc61
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2021-12-23 13:03:34 +0300

    Fix react-router-dom issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router';
+import { Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Company from './pages/Company';
 
